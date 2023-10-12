52eb37089aee88448ef4740f89eaef79f2daea30
habigerallan <habigerallan1@gmail.com>
2023-10-11 20:09:50 -0500

    cleaned up game loop

--- doc/main.py
+++ doc/main.py
@@ -7,11 +7,7 @@ from nn_builder import *
 from nn_layers import *
 
 
-def train(x, y):
-	x = np.array(x)
-	y = np.array(y)
-
-	print("Training Started...")
+def create_network() -> Network:
 	net = Network()
 	net.add(FCLayer(9, 12))
 	net.add(ActivationLayer(tanh, tanh_prime))
@@ -22,78 +18,110 @@ def train(x, y):
 	net.add(FCLayer(12, 9))
 	net.add(ActivationLayer(tanh, tanh_prime))
 	net.use(mse, mse_prime)
-	net.fit(x, y, epochs=1000, learning_rate=0.1)
-	print("Training Complete.")
+
 	return net
+
+
+def train(net, x, y) -> None:
+	x = np.array(x)
+	y = np.array(y)
+
+	print((f"Training on {len(x)} games..."))
+
+	net.fit(x, y, epochs=1000, learning_rate=0.1)
+
+	print("Training Complete.")
+
 
 def setup_gui():
 	pass
 
-def is_over(game, net, x, y):
+
+def is_game_over(game) -> bool:
 	if (game.complete != 0):
-		print(game)
 		print("Game Over!")
 		print("Restarting...")
-		net = train(x, y)
-		game = TTTGame()
-		return net, game, True
+		return True
+	
+	return False
 
-	return net, game, False
 
-def get_data(game):
+def get_data(game) -> (list, list):
 	x_temp = game.get_board_copy()
 	y_temp = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-	i = find_best_move(game)
-	y_temp[i] = 1
+	index = find_best_move(game)
+	y_temp[index] = 1
 
 	return x_temp, y_temp
 
-def game_loop():
+
+def game_loop() -> None:
 	game = TTTGame()
-	net = None
+	net = create_network()
 
 	x_train = []
 	y_train = []
 
 	while (True):
-		net, game, over = is_over(game, net, x_train, y_train)
-		if (over):
+
+
+		# Game Handling
+		print(game)
+
+		if (is_game_over(game)):
+			game = TTTGame()
+			train(net, x_train, y_train)
 			continue
 		
 		x_temp, y_temp = get_data(game)
 		x_train.append(x_temp)
 		y_train.append(y_temp)
 
+		
+		# Player Move
+		move_lst = range(9)
+
+		try: 
+			move = int(input("Enter your move: "))
+		except ValueError:
+			move = random.choice(move_lst)
+		
+		if (move < 0 and 8 < move):
+			move = random.choice(move_lst)
+
+		while (game.make_move(move) == -1):
+			move = random.choice(move_lst)
+
+		print(f"Picked move: {move}")
+		print(f"Best move: {find_best_move(game)}")
+
+
+		# Game Handling
 		print(game)
 
-		move = int(input("Enter your move: "))
-		game.make_move(move)
-
-		net, game, over = is_over(game, net, x_train, y_train)
-		if (over):
+		if (is_game_over(game)):
+			game = TTTGame()
+			train(net, x_train, y_train)
 			continue
-
+		
 		x_temp, y_temp = get_data(game)
 		x_train.append(x_temp)
 		y_train.append(y_temp)
 
-		if (net):
-			move_data = net.predict(np.array(game.board))
-			move = np.argmax(move_data)
-			print(f"picked move: {move}")
-			print(f"best move: {find_best_move(game)}")
-				
-			move_lst = range(9)
-			while (game.make_move(move) == -1):
-				move = random.choice(move_lst)
-		else:
-			move_lst = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+		# AI Move
+		move_data = net.predict(np.array(game.board))
+		move = np.argmax(move_data)
+
+		move_lst = range(9)
+		while (game.make_move(move) == -1):
 			move = random.choice(move_lst)
-			while (game.make_move(move) == -1):
-				move_lst.remove(move)
-				move = random.choice(move_lst)
-	
+
+		print(f"Picked move: {move}")
+		print(f"Best move: {find_best_move(game)}")
+
+
 def main():
 	setup_gui()
 	game_loop()
