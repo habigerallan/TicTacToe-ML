132d7819f23044f33b504a0a1d306bfe5aa93371
habigerallan <habigerallan1@gmail.com>
2023-10-18 14:09:53 -0500

    RAHHHHHH

--- doc/app.py
+++ doc/app.py
@@ -11,81 +11,32 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.label import Label
 from kivy.graphics import Color, Rectangle, Line, Ellipse
 
-import random
-import numpy as np
+class TicTacToeApp(App):
+    def __init__(self):
+        self.game_layout = GameLayout()
 
-from tictactoe import *
-from nn_builder import *
-from nn_layers import *
+    def build(self):
+        Window.size = (640, 360)
 
-def create_network() -> Network:
-	net = Network()
-	net.add(FCLayer(9, 12))
-	net.add(ActivationLayer(tanh, tanh_prime))
-	net.add(FCLayer(12, 21))
-	net.add(ActivationLayer(tanh, tanh_prime))
-	net.add(FCLayer(21, 12))
-	net.add(ActivationLayer(tanh, tanh_prime))
-	net.add(FCLayer(12, 9))
-	net.add(ActivationLayer(tanh, tanh_prime))
-	net.use(mse, mse_prime)
+        root = Root()
+        root_bg = Background(color=(1,1,1,1), parent=root)
+        root.add_widget(root_bg)
 
-	return net
+        game_frame = GameFrame()
+        game_frame_bg = Background(color=(0,0,0,.1), parent=game_frame)
+        game_frame.add_widget(game_frame_bg)
+        root.add_widget(game_frame)
 
-
-def train(net, x, y) -> None:
-	x = np.array(x)
-	y = np.array(y)
-
-	print((f"Training on {len(x)} games..."))
-
-	net.fit(x, y, epochs=1000, learning_rate=0.1)
-
-
-def setup_gui():
-	pass
-
-
-def is_game_over(game) -> bool:
-	if (game.complete != 0):
-		print("Game Over!")
-		print("Restarting...")
-		return True
-	
-	return False
-
-
-def get_data(game) -> (list, list):
-	x_temp = game.get_board_copy()
-	y_temp = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-	index = find_best_move(game)
-	y_temp[index] = 1
-
-	return x_temp, y_temp
+        game_frame.add_widget(self.game_layout)
+        return root
 
 class Root(RelativeLayout):
     def __init__(self, **kwargs):
         super(Root, self).__init__(**kwargs)
         self.size = Window.size
         self.pos = (0,0)
-
-class Background(Widget):
-    def __init__(self, color, parent, **kwargs):
-        super(Background, self).__init__(**kwargs)
-
-        with self.canvas.before:
-            Color(*color)
-            self.rect = Rectangle(pos=parent.pos, size=parent.size)
-
-        self.bind(pos=self._update_rect, size=self._update_rect)
-
-    def _update_rect(self, instance, value):
-        self.rect.pos = instance.pos
-        self.rect.size = instance.size
 
 class GameFrame(RelativeLayout):
     def __init__(self, **kwargs):
@@ -100,13 +51,8 @@ class GameLayout(GridLayout):
         self.spacing = 25
         self.cols = 3
         self.rows = 3
-
-        self.game = TTTGame()
-        self.net = create_network()
-        self.x_train = []
-        self.y_train = []
-
         self.cells = []
+
         for i in range(9):
             cell = GameCell(index=i)
             self.add_widget(cell)
@@ -123,7 +69,6 @@ class GameLayout(GridLayout):
             self.add_widget(cell)
             cell.parent = self
             self.cells.append(cell)
-        self.game = TTTGame()
         
 class GameCell(Button):
     def __init__(self, index, **kwargs):
@@ -132,88 +77,40 @@ class GameCell(Button):
         self.background_down = ''
         self.background_color = (1, 1, 1, 1)
         self.index = index        
+        self.selected = False
         self.filled = False
 
         self.bind(on_press=self.mouse1_down)
 
     def mouse1_down(self, cell):
-        game = cell.parent.game
-        net = cell.parent.net
-        x_train = cell.parent.x_train
-        y_train = cell.parent.y_train
-        if (cell.filled == False and game.complete == 0):
-            cell.filled = True
+        if not cell.filled:
+            cell.selected = True
 
-            cell.draw_x(cell)
-            # PLAYER MOVE
-            x_temp, y_temp = get_data(game)
-            x_train.append(x_temp)
-            y_train.append(y_temp)
-
-            game.make_move(cell.index)
-
-            if (is_game_over(game)):
-                train(net, x_train, y_train)
-                cell.parent.clear_cells()
-                return
-
-
-            # AI MOVE
-            x_temp, y_temp = get_data(game)
-            x_train.append(x_temp)
-            y_train.append(y_temp)
-
-            move_data = net.predict(np.array(game.board))
-            move = np.argmax(move_data)
-
-            move_lst = range(9)
-            while (game.make_move(move) == -1):
-                move = random.choice(move_lst)
-
-            for i in range(9):
-                if (cell.parent.cells[i].index == move):
-                    ai_cell = cell.parent.cells[i]
-                    
-                    ai_cell.filled = True
-
-                    ai_cell.draw_o(ai_cell)
-
-                    if (is_game_over(game)):
-                        train(net, x_train, y_train)
-                        ai_cell.parent.clear_cells()
-                        return
-
-
-    def draw_x(self, cell):
-        with cell.canvas.after:
+    def draw_x(self):
+        with self.canvas.after:
             Color(0, 0, 0, 1)
-            Line(points=[cell.x, cell.y, cell.right, cell.top], width=10)
-            Line(points=[cell.x, cell.top, cell.right, cell.y], width=10)
+            Line(points=[self.x, self.y, self.right, self.top], width=10)
+            Line(points=[self.x, self.top, self.right, self.y], width=10)
 
 
-    def draw_o(self, cell):
-        with cell.canvas.after:
+    def draw_o(self):
+        with self.canvas.after:
             Color(1, 1, 1, 1)
-            Ellipse(pos=cell.pos, size=cell.size)
+            Ellipse(pos=self.pos, size=self.size)
             Color(0, 0, 0, 1)
-            Line(circle=[cell.center_x, cell.center_y, min(cell.width, cell.height) / 2], width=10)
+            Line(circle=[self.center_x, self.center_y, min(self.width, self.height) / 2], width=10)
 
-class TicTacToeApp(App):
-    def build(self):
-        Window.size = (640, 360)
+class Background(Widget):
+    def __init__(self, color, parent, **kwargs):
+        super(Background, self).__init__(**kwargs)
 
-        root = Root()
-        root_bg = Background(color=(1,1,1,1), parent=root)
-        root.add_widget(root_bg)
+        with self.canvas.before:
+            Color(*color)
+            self.rect = Rectangle(pos=parent.pos, size=parent.size)
 
-        game_frame = GameFrame()
-        game_frame_bg = Background(color=(0,0,0,.1), parent=game_frame)
-        game_frame.add_widget(game_frame_bg)
-        root.add_widget(game_frame)
+        self.bind(pos=self._update_rect, size=self._update_rect)
 
-        game_layout = GameLayout()
-        game_frame.add_widget(game_layout)
-        return root
-
-if __name__ == '__main__':
-    TicTacToeApp().run()
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
+    
--- doc/main.py
+++ doc/main.py
@@ -1,10 +1,12 @@
 import random
+import threading
 
 import numpy as np
 
 from tictactoe import *
 from nn_builder import *
 from nn_layers import *
+from app import *
 
 
 def create_network() -> Network:
@@ -30,11 +32,6 @@ def train(net, x, y) -> None:
 
 	net.fit(x, y, epochs=1000, learning_rate=0.1)
 
-
-def setup_gui():
-	pass
-
-
 def is_game_over(game) -> bool:
 	if (game.complete != 0):
 		print("Game Over!")
@@ -53,8 +50,13 @@ def get_data(game) -> (list, list):
 
 	return x_temp, y_temp
 
+def setup_gui() -> object:
+	return TicTacToeApp()
 
-def game_loop() -> None:
+def game_loop(ttt_app) -> None:
+	app = ttt_app
+	game_layout = app.game_layout
+
 	game = TTTGame()
 	net = create_network()
 
@@ -62,8 +64,6 @@ def game_loop() -> None:
 	y_train = []
 	
 	while (True):
-
-
 		# Game Handling
 		print(game)
 
@@ -76,23 +76,39 @@ def game_loop() -> None:
 		x_train.append(x_temp)
 		y_train.append(y_temp)
 
-		
+		made_move = False
+		while made_move:
+			for i in range(9): 
+				if game_layout.cells[i].selected:
+					game_layout.cells[i].selected = False
+					game_layout.cells[i].filled = True
+
+					if game.move_value == 1:
+						game_layout.cells[i].draw_x()
+					else:
+						game_layout.cells[i].draw_o()
+
+					game.make_move(i)
+					made_move = True
+
+					break
+
 		# Player Move
-		move_lst = range(9)
+		# move_lst = range(9)
 
-		try: 
-			move = int(input("Enter your move: "))
-		except ValueError:
-			move = random.choice(move_lst)
+		# try: 
+		# 	move = int(input("Enter your move: "))
+		# except ValueError:
+		# 	move = random.choice(move_lst)
 		
-		if (move < 0 or 8 < move):
-			move = random.choice(move_lst)
+		# if (move < 0 or 8 < move):
+		# 	move = random.choice(move_lst)
 
-		while (game.make_move(move) == -1):
-			move = random.choice(move_lst)
+		# while (game.make_move(move) == -1):
+		# 	move = random.choice(move_lst)
 
-		print(f"Picked move: {move}")
-		print(f"Best move: {find_best_move(game)}")
+		# print(f"Picked move: {move}")
+		# print(f"Best move: {find_best_move(game)}")
 
 
 		# Game Handling
@@ -113,16 +129,28 @@ def game_loop() -> None:
 		move = np.argmax(move_data)
 
 		move_lst = range(9)
-		while (game.make_move(move) == -1):
+		while (game.board[move] != 0):
 			move = random.choice(move_lst)
 
+		game_layout.cells[move].selected = False
+		game_layout.cells[move].filled = True
+
+		if game.move_value == 1:
+			game_layout.cells[move].draw_x()
+		else:
+			game_layout.cells[move].draw_o()
+
+		game.make_move(move)
+		
 		print(f"Picked move: {move}")
 		print(f"Best move: {find_best_move(game)}")
 
 
 def main() -> None:
-	setup_gui()
-	game_loop()
+	app = setup_gui()
+	threading.Thread(target=game_loop, args=[app]).start()
+	app.run()
+	
 
 
 if __name__ == '__main__':
